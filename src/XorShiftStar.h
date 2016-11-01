2784525c31abd5964f2f550a02482dde44c095a8
Martin Ankerl <martin.ankerl@gmail.com>
2016-11-01 19:08:16 +0100

    switched to 64bit

--- src/XorShiftStar.h
+++ src/XorShiftStar.h
@@ -7,7 +7,7 @@
 /// @see https://en.wikipedia.org/wiki/Xorshift#xorshift.2A
 class XorShiftStar {
 public:
-    typedef uint32_t result_type;
+    typedef uint64_t result_type;
 
     // random state created at https://www.random.org/cgi-bin/randbyte?nbytes=8&format=h
     XorShiftStar(uint64_t initialState = 0x853c49e6748fea9bULL)
@@ -18,34 +18,36 @@ public:
         mState = state;
     }
 
-    inline uint32_t operator()() {
+    // 563.031 Million OPS for XorShiftStar
+    // 548.254 ILP
+    inline uint64_t operator()() {
         mState ^= mState >> 12; // a
         mState ^= mState << 25; // b
         mState ^= mState >> 27; // c
-        return static_cast<uint32_t>(mState * 2685821657736338717ULL);
+        return mState * UINT64_C(2685821657736338717);
     }
 
     /// Random float value between 0 and 1.
-    inline float rand01() {
+    inline double rand01() {
         // 1.0 / (2^32 - 1)
-        return operator()() * 2.3283064370807973754314699618685e-10f;
+        return operator()() * 5.4210108624275221703311375920553e-20;
     }
 
     /// Random float value between min and max.
-    inline float operator()(float min_val, float max_val) {
+    inline double operator()(double min_val, double max_val) {
         return (max_val - min_val) * rand01() + min_val;
     }
 
     // Generates using the given range. This has a modulo bias.
-    inline uint32_t operator()(size_t range) {
-        return operator()() % static_cast<uint32_t>(range);
+    inline uint64_t operator()(uint64_t range) {
+        return operator()() % range;
     }
 
-    inline static uint32_t max() {
+    inline static uint64_t max() {
         return -1;
     }
 
-    inline static uint32_t min() {
+    inline static uint64_t min() {
         return 0;
     }
 
