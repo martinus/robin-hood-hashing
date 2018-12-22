015937e8db1c818bdc74d53e316877a1398da164
Martin Ankerl <martin.ankerl@gmail.com>
2018-12-22 09:29:24 +0100

    refactoring, avalanching test added

--- src/test/test_counters.cpp
+++ src/test/test_counters.cpp
@@ -6,56 +6,65 @@
 #include <map>
 #include <unordered_map>
 
-static size_t sCountCtor = 0;
-static size_t sCountDefaultCtor = 0;
-static size_t sCountCopyCtor = 0;
-static size_t sCountDtor = 0;
-static size_t sCountEquals = 0;
-static size_t sCountLess = 0;
-static size_t sCountAssign = 0;
-static size_t sCountSwaps = 0;
-static size_t sCountGet = 0;
-static size_t sCountConstGet = 0;
-static size_t sCountHash = 0;
-static size_t sCountMoveCtor = 0;
-static size_t sCountMoveAssign = 0;
-
 static bool sHasHeaderPrinted = false;
 
-void resetStaticCounts() {
-	sCountCtor = 0;
-	sCountDefaultCtor = 0;
-	sCountCopyCtor = 0;
-	sCountDtor = 0;
-	sCountEquals = 0;
-	sCountLess = 0;
-	sCountAssign = 0;
-	sCountSwaps = 0;
-	sCountGet = 0;
-	sCountConstGet = 0;
-	sCountHash = 0;
-	sCountMoveCtor = 0;
-	sCountMoveAssign = 0;
-}
-
-void printStaticHeaderOnce() {
-	if (sHasHeaderPrinted) {
-		return;
-	}
-	printf("    ctor   defctor  cpyctor     dtor   assign    swaps      get  cnstget     hash   equals     less   ctormv assignmv |   total\n");
-	sHasHeaderPrinted = true;
-}
-
-void printStaticCounts(std::string const& title) {
-	size_t total = sCountCtor + sCountDefaultCtor + sCountCopyCtor + sCountDtor + sCountEquals + sCountLess + sCountAssign + sCountSwaps + sCountGet +
-				   sCountConstGet + sCountHash + sCountMoveCtor + sCountMoveAssign;
-
-	printf("%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu |%9zu %s\n", sCountCtor, sCountDefaultCtor, sCountCopyCtor, sCountDtor, sCountAssign,
-		   sCountSwaps, sCountGet, sCountConstGet, sCountHash, sCountEquals, sCountLess, sCountMoveCtor, sCountMoveAssign, total, title.c_str());
-}
-
 class Counter {
+	static size_t sCountCtor;
+	static size_t sCountDefaultCtor;
+	static size_t sCountCopyCtor;
+	static size_t sCountDtor;
+	static size_t sCountEquals;
+	static size_t sCountLess;
+	static size_t sCountAssign;
+	static size_t sCountSwaps;
+	static size_t sCountGet;
+	static size_t sCountConstGet;
+	static size_t sCountHash;
+	static size_t sCountMoveCtor;
+	static size_t sCountMoveAssign;
+
 public:
+	static void resetStaticCounts() {
+		sCountCtor = 0;
+		sCountDefaultCtor = 0;
+		sCountCopyCtor = 0;
+		sCountDtor = 0;
+		sCountEquals = 0;
+		sCountLess = 0;
+		sCountAssign = 0;
+		sCountSwaps = 0;
+		sCountGet = 0;
+		sCountConstGet = 0;
+		sCountHash = 0;
+		sCountMoveCtor = 0;
+		sCountMoveAssign = 0;
+	}
+
+	static void printStaticHeaderOnce() {
+		if (sHasHeaderPrinted) {
+			return;
+		}
+		printf("    ctor   defctor  cpyctor     dtor   assign    swaps      get  cnstget     hash   equals     less   ctormv assignmv |   total\n");
+		sHasHeaderPrinted = true;
+	}
+
+	static void printStaticCounts(std::string const& title) {
+		size_t total = sCountCtor + sCountDefaultCtor + sCountCopyCtor + sCountDtor + sCountEquals + sCountLess + sCountAssign + sCountSwaps +
+					   sCountGet + sCountConstGet + sCountHash + sCountMoveCtor + sCountMoveAssign;
+
+		printf("%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu%9zu |%9zu %s\n", sCountCtor, sCountDefaultCtor, sCountCopyCtor, sCountDtor,
+			   sCountAssign, sCountSwaps, sCountGet, sCountConstGet, sCountHash, sCountEquals, sCountLess, sCountMoveCtor, sCountMoveAssign, total,
+			   title.c_str());
+	}
+
+	static size_t countCtor() {
+		return sCountCtor + sCountDefaultCtor + sCountCopyCtor + sCountMoveCtor;
+	}
+
+	static size_t countDtor() {
+		return sCountDtor;
+	}
+
 	Counter(const int& data)
 		: mData(data) {
 		++sCountCtor;
@@ -118,9 +127,28 @@ public:
 		swap(mData, other.mData);
 	}
 
+	size_t hash() const {
+		++sCountHash;
+		return std::hash<int>{}(mData);
+	}
+
 private:
 	int mData;
 };
+
+size_t Counter::sCountCtor{};
+size_t Counter::sCountDefaultCtor{};
+size_t Counter::sCountCopyCtor{};
+size_t Counter::sCountDtor{};
+size_t Counter::sCountEquals{};
+size_t Counter::sCountLess{};
+size_t Counter::sCountAssign{};
+size_t Counter::sCountSwaps{};
+size_t Counter::sCountGet{};
+size_t Counter::sCountConstGet{};
+size_t Counter::sCountHash{};
+size_t Counter::sCountMoveCtor{};
+size_t Counter::sCountMoveAssign{};
 
 void swap(Counter& a, Counter& b) {
 	a.swap(b);
@@ -132,8 +160,7 @@ template <>
 class hash<Counter> {
 public:
 	size_t operator()(const Counter& c) const {
-		++sCountHash;
-		return hash<int>()(c.get());
+		return c.hash();
 	}
 };
 
@@ -162,37 +189,37 @@ const char* name(robin_hood::node_map<K, V> const&) {
 TEMPLATE_TEST_CASE("map ctor & dtor", "[display]", (std::map<Counter, Counter>), (std::unordered_map<Counter, Counter>),
 				   (robin_hood::flat_map<Counter, Counter>), (robin_hood::node_map<Counter, Counter>)) {
 
-	printStaticHeaderOnce();
+	Counter::printStaticHeaderOnce();
 
-	resetStaticCounts();
+	Counter::resetStaticCounts();
 	{ TestType map; }
-	printStaticCounts(std::string("ctor & dtor ") + name(TestType{}));
-	REQUIRE(sCountDtor == sCountCtor + sCountDefaultCtor + sCountCopyCtor + sCountMoveCtor);
-	REQUIRE(sCountDtor == 0);
-	resetStaticCounts();
+	Counter::printStaticCounts(std::string("ctor & dtor ") + name(TestType{}));
+	REQUIRE(Counter::countDtor() == Counter::countCtor());
+	REQUIRE(Counter::countDtor() == 0);
+	Counter::resetStaticCounts();
 }
 
 TEMPLATE_TEST_CASE("1 emplace", "[display]", (std::map<Counter, Counter>), (std::unordered_map<Counter, Counter>),
 				   (robin_hood::flat_map<Counter, Counter>), (robin_hood::node_map<Counter, Counter>)) {
 
-	printStaticHeaderOnce();
+	Counter::printStaticHeaderOnce();
 
-	resetStaticCounts();
+	Counter::resetStaticCounts();
 	{
 		TestType map;
 		map.emplace(1, 2);
 	}
-	printStaticCounts(std::string("1 emplace ") + name(TestType{}));
-	REQUIRE(sCountDtor == sCountCtor + sCountDefaultCtor + sCountCopyCtor + sCountMoveCtor);
-	resetStaticCounts();
+	Counter::printStaticCounts(std::string("1 emplace ") + name(TestType{}));
+	REQUIRE(Counter::countDtor() == Counter::countCtor());
+	Counter::resetStaticCounts();
 }
 
 TEMPLATE_TEST_CASE("10k random insert & erase", "[display]", (std::map<Counter, Counter>), (std::unordered_map<Counter, Counter>),
 				   (robin_hood::flat_map<Counter, Counter>), (robin_hood::node_map<Counter, Counter>)) {
 
-	printStaticHeaderOnce();
+	Counter::printStaticHeaderOnce();
 
-	resetStaticCounts();
+	Counter::resetStaticCounts();
 	{
 		Rng rng(321);
 		TestType map;
@@ -210,9 +237,9 @@ TEMPLATE_TEST_CASE("10k random insert & erase", "[display]", (std::map<Counter, 
 		}
 	}
 
-	printStaticCounts(std::string("10k random insert & erase - ") + name(TestType{}));
-	REQUIRE(sCountDtor == sCountCtor + sCountDefaultCtor + sCountCopyCtor + sCountMoveCtor);
-	resetStaticCounts();
+	Counter::printStaticCounts(std::string("10k random insert & erase - ") + name(TestType{}));
+	REQUIRE(Counter::countDtor() == Counter::countCtor());
+	Counter::resetStaticCounts();
 }
 
 #define PRINT_SIZEOF(x, A, B) std::cout << sizeof(x<A, B>) << " bytes for " #x "<" #A ", " #B ">" << std::endl
