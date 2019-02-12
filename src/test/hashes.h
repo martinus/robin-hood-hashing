b7b51eb93faa8de5bbd7d27328aafd88626180a2
Martin Ankerl <martin.ankerl@dynatrace.com>
2019-02-12 07:19:03 +0100

    bugfix in hashes: used the wrong constants!

--- src/test/hashes.h
+++ src/test/hashes.h
@@ -19,11 +19,11 @@ template <typename T>
 struct FNV1a {
     size_t operator()(T const& key) const {
 #if SIZE_MAX == UINT32_MAX
-        static constexpr size_t FNV_offset_basis = UINT64_C(14695981039346656037);
-        static constexpr size_t FNV_prime = UINT64_C(1099511628211);
-#else
         static constexpr size_t FNV_offset_basis = UINT32_C(2166136261);
         static constexpr size_t FNV_prime = UINT32_C(16777619);
+#else
+        static constexpr size_t FNV_offset_basis = UINT64_C(14695981039346656037);
+        static constexpr size_t FNV_prime = UINT64_C(1099511628211);
 #endif
         auto const data = reinterpret_cast<uint8_t const*>(&key);
         size_t val = FNV_offset_basis;
