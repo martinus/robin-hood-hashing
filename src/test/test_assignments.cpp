f63d557ebd2956993f082f5770c2f292b01d6e35
Martin Ankerl <martin.ankerl@dynatrace.com>
2019-04-16 12:36:11 +0200

    add test to demonstrate non copyable/movable

--- src/test/test_assignments.cpp
+++ src/test/test_assignments.cpp
@@ -186,3 +186,38 @@ TEMPLATE_TEST_CASE("reserve", "", FlatMap, NodeMap) {
     map.reserve(820);
     REQUIRE(2047 == map.mask());
 }
+
+struct NonCopyable {
+    ~NonCopyable() = default;
+    NonCopyable(NonCopyable const&) = delete;
+    NonCopyable& operator=(NonCopyable const&) = delete;
+
+    NonCopyable(NonCopyable&&) = default;
+    NonCopyable& operator=(NonCopyable&&) = default;
+};
+
+struct NonCopyMove {
+    NonCopyMove(int) {}
+
+    NonCopyMove(NonCopyMove const&) = delete;
+    NonCopyMove& operator=(NonCopyMove const&) = delete;
+
+    NonCopyMove(NonCopyMove&&) = delete;
+    NonCopyMove& operator=(NonCopyMove&&) = delete;
+};
+
+TEST_CASE("noncopyable") {
+    // it's ok because it is movable.
+    robin_hood::unordered_flat_map<int, NonCopyable> m;
+    m[1];
+}
+
+TEST_CASE("nonmovable") {
+    // not ok: can't copy and can't move
+    // robin_hood::unordered_flat_map<int, NonCopyMove> m2;
+    // m2.emplace(std::piecewise_construct, std::forward_as_tuple(123), std::forward_as_tuple(333));
+
+    // it's ok because unordered_map defaults to unordered_node_map
+    robin_hood::unordered_map<int, NonCopyMove> m;
+    m.emplace(std::piecewise_construct, std::forward_as_tuple(123), std::forward_as_tuple(333));
+}
