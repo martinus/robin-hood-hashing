470b2028fcb0ca43e74fef2ccd0c4073a0e2c660
Martin Ankerl <martin.ankerl@gmail.com>
2019-04-28 19:35:43 +0200

    nicer benchmark output

--- src/test/app/benchmark.cpp
+++ src/test/app/benchmark.cpp
@@ -9,5 +9,5 @@ Benchmark::~Benchmark() {
     auto runtime_sec = std::chrono::duration<double>(clock::now() - mStartTime).count();
     std::cout.precision(3);
     std::cout << std::fixed << std::setw(16) << (runtime_sec / mCount * 1e9) << " ns/" << mOpName
-              << "," << std::setw(7) << runtime_sec << " s total" << mMsg << std::endl;
+              << "," << std::setw(7) << runtime_sec << " s total " << mMsg << std::endl;
 }
