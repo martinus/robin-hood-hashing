8ca555c7318eb6f7e18d46dbdde337ec2705963a
Martin Ankerl <martin.ankerl@gmail.com>
2019-04-29 17:01:29 +0200

    renamed test output

--- src/test/unit/count_ctor_dtor_1.cpp
+++ src/test/unit/count_ctor_dtor_1.cpp
@@ -6,7 +6,7 @@
 
 #include <array>
 
-TEST_CASE_TEMPLATE("count ctor & dtor" * doctest::test_suite("count") * doctest::skip(), Map,
+TEST_CASE_TEMPLATE("count ctor-emplace-dtor" * doctest::test_suite("count") * doctest::skip(), Map,
                    robin_hood::unordered_flat_map<cnt, cnt, std::hash<cnt>>,
                    robin_hood::unordered_flat_map<cnt, cnt, robin_hood::hash<cnt>>,
                    robin_hood::unordered_node_map<cnt, cnt, std::hash<cnt>>,
@@ -19,5 +19,5 @@ TEST_CASE_TEMPLATE("count ctor & dtor" * doctest::test_suite("count") * doctest:
         Map m;
         m.emplace(counts(1), counts(2));
     }
-    counts.printCounts(std::string("ctor & dtor ") + doctest::detail::type_to_string<Map>());
+    counts.printCounts(std::string("ctor-emplace-dtor") + doctest::detail::type_to_string<Map>());
 }
