8b55a7385a55f082e65476811f0f6abb01eb0f9d
Martin Ankerl <martin.ankerl@gmail.com>
2019-04-29 21:42:35 +0200

    more tests

--- src/test/old/test_empty.cpp
+++ src/test/old/test_empty.cpp
@@ -2,20 +2,6 @@
 
 #include <memory>
 
-TEST_CASE("unique ptr") {
-    robin_hood::unordered_map<int, std::unique_ptr<int>> map;
-    map[321].reset(new int(123));
-}
-
-TEST_CASE("insert list") {
-    std::vector<robin_hood::unordered_map<int, int>::value_type> v;
-    v.emplace_back(1, 2);
-    v.emplace_back(3, 4);
-
-    robin_hood::unordered_map<int, int> map(v.begin(), v.end());
-    REQUIRE(map.size() == 2);
-}
-
 TEST_CASE("trivially copyable") {
     static_assert(
         std::is_trivially_copyable<robin_hood::unordered_map<int, int>::value_type>::value,
