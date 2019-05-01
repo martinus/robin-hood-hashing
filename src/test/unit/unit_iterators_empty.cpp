cdabec592bd57f360baf9d684f10c771db4949e3
Martin Ankerl <martin.ankerl@gmail.com>
2019-05-01 08:02:26 +0200

    iterators empty test

---
+++ src/test/unit/unit_iterators_empty.cpp
@@ -0,0 +1,22 @@
+#include <app/doctest.h>
+#include <robin_hood.h>
+
+TYPE_TO_STRING(robin_hood::unordered_flat_map<uint64_t, uint64_t>);
+TYPE_TO_STRING(robin_hood::unordered_node_map<uint64_t, uint64_t>);
+
+TEST_CASE_TEMPLATE("iterators empty", Map, robin_hood::unordered_flat_map<uint64_t, size_t>,
+                   robin_hood::unordered_node_map<uint64_t, size_t>) {
+    for (size_t i = 0; i < 10; ++i) {
+        Map m;
+        REQUIRE(m.begin() == m.end());
+
+        REQUIRE(m.end() == m.find(132));
+
+        m[1];
+        REQUIRE(m.begin() != m.end());
+        REQUIRE(++m.begin() == m.end());
+        m.clear();
+
+        REQUIRE(m.begin() == m.end());
+    }
+}
