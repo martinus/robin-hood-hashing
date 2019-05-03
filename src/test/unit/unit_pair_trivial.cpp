1287cbb5d335afc89e4ef448e74db4f14d8c06be
Martin Ankerl <martin.ankerl@gmail.com>
2019-05-03 13:37:46 +0200

    better error check in pair triviallity

--- src/test/unit/unit_pair_trivial.cpp
+++ src/test/unit/unit_pair_trivial.cpp
@@ -7,29 +7,36 @@
 #include <type_traits>
 
 template <typename K, typename V>
-void nothrow_trivially(bool is_nothrow, bool is_trivially) {
-    REQUIRE(std::is_nothrow_move_constructible<robin_hood::pair<K, V>>::value == is_nothrow);
-    REQUIRE(std::is_nothrow_move_assignable<robin_hood::pair<K, V>>::value == is_nothrow);
+using TestPair = robin_hood::pair<K, V>;
 
-    REQUIRE(std::is_trivially_copyable<robin_hood::pair<K, V>>::value == is_trivially);
-    REQUIRE(std::is_trivially_destructible<robin_hood::pair<K, V>>::value == is_trivially);
+template <typename K, typename V>
+void nothrow_trivially() {
 
-    REQUIRE(sizeof(robin_hood::pair<K, V>) == sizeof(std::pair<K, V>));
-    /*
-    std::cout << std::is_trivially_copyable<std::pair<K, V>>::value << " "
-              << std::is_trivially_destructible<std::pair<K, V>>::value << ", expected: " << result
-              << std::endl;
-    */
+    REQUIRE(std::is_nothrow_move_constructible<TestPair<K, V>>::value ==
+            (std::is_nothrow_move_constructible<K>::value &&
+             std::is_nothrow_move_constructible<V>::value));
+
+    REQUIRE(
+        std::is_nothrow_move_assignable<TestPair<K, V>>::value ==
+        (std::is_nothrow_move_assignable<K>::value && std::is_nothrow_move_assignable<V>::value));
+
+    REQUIRE(std::is_trivially_copyable<TestPair<K, V>>::value ==
+            (std::is_trivially_copyable<K>::value && std::is_trivially_copyable<V>::value));
+
+    REQUIRE(std::is_trivially_destructible<TestPair<K, V>>::value ==
+            (std::is_trivially_destructible<K>::value && std::is_trivially_destructible<V>::value));
+
+    REQUIRE(sizeof(TestPair<K, V>) == sizeof(std::pair<K, V>));
 }
 
 TEST_CASE("pair trivially copy/destructible") {
-    nothrow_trivially<int, int>(true, true);
-    nothrow_trivially<float, float>(true, true);
-    nothrow_trivially<size_t, std::string>(true, false);
-    nothrow_trivially<std::string, size_t>(true, false);
-    nothrow_trivially<char const*, size_t>(true, true);
-    nothrow_trivially<size_t, char const*>(true, true);
-    nothrow_trivially<uint64_t, uint64_t>(true, true);
-    nothrow_trivially<uint32_t, uint32_t>(true, true);
-    nothrow_trivially<char, char>(true, true);
+    nothrow_trivially<int, int>();
+    nothrow_trivially<float, float>();
+    nothrow_trivially<size_t, std::string>();
+    nothrow_trivially<std::string, size_t>();
+    nothrow_trivially<char const*, size_t>();
+    nothrow_trivially<size_t, char const*>();
+    nothrow_trivially<uint64_t, uint64_t>();
+    nothrow_trivially<uint32_t, uint32_t>();
+    nothrow_trivially<char, char>();
 }
