190de0d5adaba866c866403292bc7fbba4409642
Martin Ankerl <martin.ankerl@gmail.com>
2019-05-10 17:14:34 +0200

    don't disable on clang, it's not there

--- src/include/robin_hood.h
+++ src/include/robin_hood.h
@@ -607,7 +607,7 @@ struct hash<T*> {
         }                                                \
     }
 
-#if defined(__GNUC__)
+#if defined(__GNUC__) && !defined(__clang__)
 #    pragma GCC diagnostic push
 #    pragma GCC diagnostic ignored "-Wuseless-cast"
 #endif
