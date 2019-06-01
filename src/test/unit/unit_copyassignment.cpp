e079dff574c920f4a8726266440c587855bdd06a
Martin Ankerl <martin.ankerl@gmail.com>
2019-06-01 07:35:30 +0200

    a few more checks added

--- src/test/unit/unit_copyassignment.cpp
+++ src/test/unit/unit_copyassignment.cpp
@@ -5,7 +5,22 @@
 TEST_CASE("copyassignment") {
     robin_hood::unordered_map<std::string, std::string> map;
     robin_hood::unordered_map<std::string, std::string> tmp;
+    
     map.emplace("a", "b");
     map = tmp;
     map.emplace("c", "d");
+
+    REQUIRE(map.size() == 1);
+    REQUIRE(map["c"] == "d");
+    REQUIRE(map.size() == 1);
+
+    REQUIRE(tmp.size() == 0);
+
+    map["e"] = "f";
+    REQUIRE(map.size() == 2);
+    REQUIRE(tmp.size() == 0);
+
+    tmp["g"] = "h";
+    REQUIRE(map.size() == 2);
+    REQUIRE(tmp.size() == 1);
 }
