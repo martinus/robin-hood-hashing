1eac612090fa15fe25b5714b39ddb3c2380bfe5e
Martin Ankerl <martin.ankerl@gmail.com>
2019-06-10 13:26:45 +0200

    clang++ 8 fixes

--- src/test/unit/unit_vectorofmaps.cpp
+++ src/test/unit/unit_vectorofmaps.cpp
@@ -10,12 +10,12 @@ TYPE_TO_STRING(robin_hood::unordered_flat_map<CtorDtorVerifier, CtorDtorVerifier
 TYPE_TO_STRING(robin_hood::unordered_node_map<CtorDtorVerifier, CtorDtorVerifier>);
 
 template <typename Map>
-void fill(Map& map, sfc64& rng) {
-    auto n = rng.uniform<size_t>(20);
+void fill(Map* map, sfc64* rng) {
+    auto n = rng->uniform<size_t>(20);
     for (size_t i = 0; i < n; ++i) {
-        auto a = rng.uniform<uint64_t>(20);
-        auto b = rng.uniform<uint64_t>(20);
-        map[a] = b;
+        auto a = rng->uniform<uint64_t>(20);
+        auto b = rng->uniform<uint64_t>(20);
+        (*map)[a] = b;
     }
 }
 
@@ -29,21 +29,21 @@ TEST_CASE_TEMPLATE("vectormap", Map,
         // copies
         for (size_t i = 0; i < 10; ++i) {
             Map m;
-            fill(m, rng);
+            fill(&m, &rng);
             maps.push_back(m);
         }
 
         // move
         for (size_t i = 0; i < 10; ++i) {
             Map m;
-            fill(m, rng);
+            fill(&m, &rng);
             maps.push_back(std::move(m));
         }
 
         // emplace
         for (size_t i = 0; i < 10; ++i) {
             maps.emplace_back();
-            fill(maps.back(), rng);
+            fill(&maps.back(), &rng);
         }
     }
     REQUIRE(CtorDtorVerifier::mapSize() == static_cast<size_t>(0));
