2317feed94ef44a2c1b73fde4b64d63d0c97e149
Martin Ankerl <martin.ankerl@gmail.com>
2019-06-28 20:50:29 +0200

    lets try if xenial works

--- src/scripts/test_all.sh
+++ src/scripts/test_all.sh
@@ -20,11 +20,23 @@ function build() {
     cmake --build .
 
     ./rh -ns -ts=show
-    
+
     ./rh
 
     cd ${ORIGINDIR}
 }
+
+build "g++-5" "11" "OFF"
+build "g++-5" "14" "OFF"
+build "g++-5" "17" "OFF"
+
+build "g++-6" "11" "OFF"
+build "g++-6" "14" "OFF"
+build "g++-6" "17" "OFF"
+
+build "g++-7" "11" "OFF"
+build "g++-7" "14" "OFF"
+build "g++-7" "17" "OFF"
 
 build "g++-4.9" "11" "OFF"
 build "g++-4.9" "14" "OFF"
