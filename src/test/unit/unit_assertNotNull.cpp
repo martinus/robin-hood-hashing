7b713c10797efa60fa3626487a8a7ffe871b149e
Martin Ankerl <martin.ankerl@gmail.com>
2019-07-06 12:23:43 +0200

    add test for assertNotNull

---
+++ src/test/unit/unit_assertNotNull.cpp
@@ -0,0 +1,21 @@
+#include <robin_hood.h>
+
+#include <app/doctest.h>
+
+struct MyException {
+    explicit MyException(int, std::string const&) {}
+};
+
+TEST_CASE("assertNotNull") {
+    REQUIRE_THROWS_AS(
+        (robin_hood::detail::assertNotNull<std::runtime_error, uint64_t>(nullptr, "exception")),
+        std::runtime_error);
+
+    uint64_t x = 123;
+    auto a = robin_hood::detail::assertNotNull<std::bad_alloc, uint64_t>(&x);
+    REQUIRE(a == &x);
+
+    REQUIRE_THROWS_AS(
+        (robin_hood::detail::assertNotNull<MyException, uint64_t>(nullptr, 123, "asdf")),
+        MyException);
+}
