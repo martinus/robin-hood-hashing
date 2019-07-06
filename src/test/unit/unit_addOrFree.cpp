bea21c3a784f849c0b95fca227db43c804e500d0
Martin Ankerl <martin.ankerl@gmail.com>
2019-07-06 19:00:39 +0200

    adds a bulkallocator test

---
+++ src/test/unit/unit_addOrFree.cpp
@@ -0,0 +1,19 @@
+#include <robin_hood.h>
+
+#include <app/doctest.h>
+
+TEST_CASE("bulkpoolallocator addOrFree") {
+    robin_hood::detail::BulkPoolAllocator<uint64_t> pool;
+
+    // add data, some too small some large enough
+    for (size_t i = 1; i < 10; ++i) {
+        auto x = malloc(i);
+        pool.addOrFree(x, i);
+    }
+
+    for (size_t i = 0; i < 100; ++i) {
+        REQUIRE(nullptr != pool.allocate());
+    }
+
+    // dtor should have freed everything
+}
