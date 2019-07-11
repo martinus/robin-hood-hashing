70da3eb07daed0738925073b95b59bd8b2e5c4f0
Martin Ankerl <martin.ankerl@gmail.com>
2019-07-11 19:02:50 +0200

    make sure too large values actually throws

--- src/test/unit/unit_reserve.cpp
+++ src/test/unit/unit_reserve.cpp
@@ -14,3 +14,10 @@ TEST_CASE_TEMPLATE("reserve", Map, robin_hood::unordered_flat_map<uint64_t, uint
     map.reserve(820);
     REQUIRE(2047 == map.mask());
 }
+
+TEST_CASE_TEMPLATE("reserve max", Map, robin_hood::unordered_flat_map<uint64_t, uint64_t>,
+                   robin_hood::unordered_node_map<uint64_t, uint64_t>) {
+
+    Map map;
+    REQUIRE_THROWS_AS(map.reserve((std::numeric_limits<size_t>::max)() - 2), std::overflow_error);
+}
