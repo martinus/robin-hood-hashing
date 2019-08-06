67bedae8c441e4e83a6782d1f2c035e2ceba1416
Martin Ankerl <martin.ankerl@dynatrace.com>
2019-08-06 08:07:48 +0200

    add example that shows how to prevent diamond problem

---
+++ src/test/unit/unit_diamond.cpp
@@ -0,0 +1,25 @@
+#include <robin_hood.h>
+
+#include <app/doctest.h>
+
+// struct that provides both hash and equals operator. Can't directly use this in the robin_hood::map.
+struct HashWithEqual {
+    size_t operator()(int x) const {
+        return static_cast<size_t>(x);
+    }
+
+    bool operator()(int a, int b) const {
+        return a == b;
+    }
+};
+
+// create simple wrapper classes, use these in the map
+struct WrapHash : public HashWithEqual {};
+struct WrapEquals : public HashWithEqual {};
+
+TEST_CASE("bulkpoolallocator addOrFree") {
+    robin_hood::unordered_flat_map<int, int, WrapHash, WrapEquals> map;
+    map[1] = 2;
+    REQUIRE(map.size() == 1);
+    REQUIRE(map.find(1) != map.end());
+}
