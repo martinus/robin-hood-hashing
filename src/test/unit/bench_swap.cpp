22adffb6956f79a51a2a9230e5ac8e233325d5a8
Martin Ankerl <martin.ankerl@gmail.com>
2020-02-22 11:40:09 +0100

    add benchmark to demonsterate complexity of swap
    As expected, swap is O(1):
    
    |   coefficient |   err% | complexity
    |--------------:|-------:|------------
    |   1.12807e-08 |   0.1% | O(1)
    |    7.2629e-10 |   6.5% | O(log n)
    |    1.6114e-13 |  46.3% | O(n)
    |   9.79843e-15 |  48.7% | O(n log n)
    |   1.71399e-18 |  64.4% | O(n^2)
    |   1.72416e-23 |  73.3% | O(n^3)

---
+++ src/test/unit/bench_swap.cpp
@@ -0,0 +1,28 @@
+#include <robin_hood.h>
+
+#include <app/benchmark.h>
+#include <app/doctest.h>
+#include <thirdparty/nanobench/nanobench.h>
+
+TYPE_TO_STRING(robin_hood::unordered_flat_map<uint64_t, uint64_t>);
+TYPE_TO_STRING(robin_hood::unordered_node_map<uint64_t, uint64_t>);
+
+TEST_CASE_TEMPLATE("bench_swap" * doctest::test_suite("nanobench") * doctest::skip(), Map,
+                   robin_hood::unordered_flat_map<uint64_t, uint64_t>,
+                   robin_hood::unordered_node_map<uint64_t, uint64_t>) {
+
+    Map a;
+    Map b;
+    ankerl::nanobench::Rng rng(123);
+
+    ankerl::nanobench::Bench bench;
+    for (size_t i = 0; i < 10; ++i) {
+        for (size_t j = 0; j < 10000; ++j) {
+            a[rng()];
+            b[rng()];
+        }
+        bench.complexityN(a.size()).run("swap " + type_string(a), [&] { std::swap(a, b); });
+    }
+    ankerl::nanobench::doNotOptimizeAway(a, b);
+    std::cout << bench.complexityBigO() << std::endl;
+}
