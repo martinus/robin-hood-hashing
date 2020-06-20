674da8f4fa12f0ef6dbbde9f07ec8229b48daab5
Martin Ankerl <martin.ankerl@gmail.com>
2020-06-20 17:03:37 +0200

    add overflow test (disabled)

--- src/test/unit/unit_overflow_collisions.cpp
+++ src/test/unit/unit_overflow_collisions.cpp
@@ -2,7 +2,12 @@
 
 #include <app/CtorDtorVerifier.h>
 #include <app/doctest.h>
+#include <app/fmt/hex.h>
 #include <app/hash/Bad.h>
+#include <app/sfc64.h>
+
+#include <iostream>
+#include <unordered_set>
 
 TEST_CASE_TEMPLATE(
     "collisions", Map,
@@ -42,4 +47,50 @@ TEST_CASE_TEMPLATE(
         CtorDtorVerifier::printMap();
     }
     REQUIRE(CtorDtorVerifier::mapSize() == 0);
+}
+
+namespace {
+
+bool isColliding(uint64_t x) {
+    return UINT64_C(0) == (x & UINT64_C(0x1fffff));
+}
+
+} // namespace
+
+TEST_CASE("overflow_finder" * doctest::skip()) {
+    robin_hood::unordered_set<uint64_t> set;
+
+    // loop until we get an overflow
+    // auto mix = UINT64_C(0x73e6d3e6f41f53a9);
+    for (uint64_t i = 0; i < UINT64_C(1000000000); ++i) {
+        auto s = i;
+        auto h1 = robin_hood::hash<uint64_t>{}(s);
+        auto h2 = h1;
+        h1 >>= 32;
+        // robin_hood::hash<uint64_t>{}(s * mix);
+
+        if (isColliding(h1)) {
+            std::cout << i << ": " << fmt::hex(s) << " -> " << fmt::hex(h1) << " " << fmt::hex(h2)
+                      << " " << isColliding(h2) << std::endl;
+            set.insert(s);
+        }
+    }
+}
+
+TEST_CASE("overflow_finder_simple" * doctest::skip()) {
+    robin_hood::unordered_set<uint64_t> set;
+    size_t count = 0;
+    uint64_t i = 0;
+    try {
+        while (count < 255) {
+            auto h = robin_hood::hash<uint64_t>{}(i);
+            if (UINT64_C(0) == (h & UINT64_C(0x1fffff))) {
+                ++count;
+                set.insert(i);
+            }
+            ++i;
+        }
+    } catch (std::overflow_error&) {
+        FAIL("i=" << i << ", count=" << count);
+    }
 }
