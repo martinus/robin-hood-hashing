e81b21931efa63e094addf3c4439aa6bd0c340db
Martin Ankerl <martin.ankerl@gmail.com>
2020-06-21 19:16:17 +0200

    fixed count

--- src/include/robin_hood.h
+++ src/include/robin_hood.h
@@ -1337,7 +1337,7 @@ private:
 
         idx = startIdx;
         while (idx != insertion_idx) {
-            ROBIN_HOOD_COUNT(shiftUp);
+            ROBIN_HOOD_COUNT(shiftUp)
             mInfo[idx] = static_cast<uint8_t>(mInfo[idx - 1] + mInfoInc);
             if (ROBIN_HOOD_UNLIKELY(mInfo[idx] + mInfoInc > 0xFF)) {
                 mMaxNumElementsAllowed = 0;
@@ -1353,7 +1353,7 @@ private:
 
         // until we find one that is either empty or has zero offset.
         while (mInfo[idx + 1] >= 2 * mInfoInc) {
-            ROBIN_HOOD_COUNT(shiftDown);
+            ROBIN_HOOD_COUNT(shiftDown)
             mInfo[idx] = static_cast<uint8_t>(mInfo[idx + 1] - mInfoInc);
             mKeyVals[idx] = std::move(mKeyVals[idx + 1]);
             ++idx;
