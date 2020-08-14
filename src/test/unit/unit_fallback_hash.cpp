569deefef5c44b58d33b0888206e69817ce11d32
Martin Ankerl <martin.ankerl@gmail.com>
2020-08-14 19:35:38 +0200

    minor test for fallback_hash

---
+++ src/test/unit/unit_fallback_hash.cpp
@@ -0,0 +1,16 @@
+#include <robin_hood.h>
+
+#include <app/doctest.h>
+
+TEST_CASE("fallback_hash") {
+    std::string str = "hello, world!";
+    auto h1 = robin_hood::fallback_hash_bytes(str.data(), str.size());
+    ++str.back();
+    auto h2 = robin_hood::fallback_hash_bytes(str.data(), str.size());
+    --str.back();
+    ++str.front();
+    auto h3 = robin_hood::fallback_hash_bytes(str.data(), str.size());
+    REQUIRE(h1 != h2);
+    REQUIRE(h1 != h3);
+    REQUIRE(h2 != h3);
+}
