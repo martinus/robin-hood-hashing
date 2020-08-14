a479bc4096a8ccf6d1911b3da371d2d0ec096dd1
Wyatt OʼDay <wyatt@wyday.com>
2020-08-14 07:46:11 -0400

    Fix CRC32 support on Windows ARM64.

--- src/include/robin_hood.h
+++ src/include/robin_hood.h
@@ -770,17 +770,18 @@ static size_t fallback_hash_bytes(void const* ptr, size_t const len) noexcept {
 
 #if ROBIN_HOOD(HAS_CRC32)
 
+#    ifndef _M_ARM64
 // see e.g.
 // https://github.com/simdjson/simdjson/blob/9863f62321f59d73c7731d4ada2d7c4ed6a0a251/src/isadetection.h
 static inline void cpuid(uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uint32_t* edx) {
-#    if defined(_MSC_VER)
+#        if defined(_MSC_VER)
     int cpuInfo[4];
     __cpuid(cpuInfo, static_cast<int>(*eax));
     *eax = static_cast<uint32_t>(cpuInfo[0]);
     *ebx = static_cast<uint32_t>(cpuInfo[1]);
     *ecx = static_cast<uint32_t>(cpuInfo[2]);
     *edx = static_cast<uint32_t>(cpuInfo[3]);
-#    else
+#        else
     uint32_t a = *eax;
     uint32_t b{};
     uint32_t c = *ecx;
@@ -791,8 +792,9 @@ static inline void cpuid(uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uint32_t* 
     *ebx = b;
     *ecx = c;
     *edx = d;
-#    endif
+#        endif
 }
+#    endif
 
 inline bool checkCrc32Support() noexcept {
 #    if defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64)
@@ -812,6 +814,8 @@ inline bool checkCrc32Support() noexcept {
     if (hwcap != ENOENT) {
         return (hwcap & HWCAP_CRC32) != 0;
     }
+#    elif defined(_M_ARM64)
+    return true;
 #    endif
     return false;
 }
