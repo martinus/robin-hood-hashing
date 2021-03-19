44332d91779314e2ea88be7b8269dfa5d3db2dd4
Martin Ankerl <martin.ankerl@gmail.com>
2021-03-19 09:06:50 +0100

    int64_t

--- src/test/unit/unit_initializer_list_insert.cpp
+++ src/test/unit/unit_initializer_list_insert.cpp
@@ -2,9 +2,6 @@
 
 #include <app/doctest.h>
 
-TYPE_TO_STRING(robin_hood::unordered_flat_map<int, int>);
-TYPE_TO_STRING(robin_hood::unordered_node_map<int, int>);
-
 TEST_CASE_TEMPLATE("insert_initializer_list", Map, robin_hood::unordered_flat_map<int, int>,
                    robin_hood::unordered_node_map<int, int>) {
     auto m = Map();
@@ -16,8 +13,8 @@ TEST_CASE_TEMPLATE("insert_initializer_list", Map, robin_hood::unordered_flat_ma
 }
 
 TEST_CASE_TEMPLATE("insert_initializer_list_string", Map,
-                   robin_hood::unordered_flat_map<uint64_t, std::string>,
-                   robin_hood::unordered_node_map<uint64_t, std::string>) {
+                   robin_hood::unordered_flat_map<int64_t, std::string>,
+                   robin_hood::unordered_node_map<int64_t, std::string>) {
     auto m = Map();
     m.insert({{1, "a"}, {3, "b"}, {5, "c"}});
     REQUIRE(m.size() == 3U);
