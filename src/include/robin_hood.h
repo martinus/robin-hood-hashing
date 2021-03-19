36e5eb80f02a894e4cc8509be888869fa475f9f0
Martin Ankerl <martin.ankerl@gmail.com>
2021-03-19 08:51:36 +0100

    updated copyright dates

--- src/include/robin_hood.h
+++ src/include/robin_hood.h
@@ -10,7 +10,7 @@
 //
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2020 Martin Ankerl <http://martin.ankerl.com>
+// Copyright (c) 2018-2021 Martin Ankerl <http://martin.ankerl.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
