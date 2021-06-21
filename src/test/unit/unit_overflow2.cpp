8d27714ca77234494a4e14e172716e7cec23a116
Martin Ankerl <martin.ankerl@gmail.com>
2021-06-21 13:00:56 +0200

    Don't change hash multiplier when hash works well, this introduces the danger of getting an overflow

---
+++ src/test/unit/unit_overflow2.cpp
@@ -0,0 +1,23 @@
+#define ROBIN_HOOD_TRACE_ENABLED
+#define ROBIN_HOOD_LOG_ENABLED
+#include <robin_hood.h>
+
+#include <app/doctest.h>
+
+#include <fstream>
+#include <iostream>
+
+TEST_CASE("bug_overflow2") {
+    robin_hood::unordered_set<uint32_t> set;
+
+    std::ifstream fin("a.txt");
+    if (fin.is_open()) {
+        uint32_t num{};
+        size_t i = 0;
+        while (fin >> num) {
+            ++i;
+            set.insert(num);
+            std::cout << i << ": " << num << std::endl;
+        }
+    }
+}
