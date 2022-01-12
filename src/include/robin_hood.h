91f612cab8a956509311f4001159dd76080881e0
zhanglistar <zhanglinuxstar@gmail.com>
2022-01-12 23:47:19 +0800

    Use malloc + memset instead of calloc
    Using `calloc` is an overhead because only the `mInfo` array needs to be set to 0, which is only a small part of the whole allocated data. Thus, in most cases using `malloc` + `memset` should be faster.

--- src/include/robin_hood.h
+++ src/include/robin_hood.h
@@ -2324,13 +2324,14 @@ private:
 
         auto const numElementsWithBuffer = calcNumElementsWithBuffer(max_elements);
 
-        // calloc also zeroes everything
+        // malloc & zero mInfo. Faster than calloc everything.
         auto const numBytesTotal = calcNumBytesTotal(numElementsWithBuffer);
         ROBIN_HOOD_LOG("std::calloc " << numBytesTotal << " = calcNumBytesTotal("
                                       << numElementsWithBuffer << ")")
         mKeyVals = reinterpret_cast<Node*>(
-            detail::assertNotNull<std::bad_alloc>(std::calloc(1, numBytesTotal)));
+            detail::assertNotNull<std::bad_alloc>(std::malloc(numBytesTotal)));
         mInfo = reinterpret_cast<uint8_t*>(mKeyVals + numElementsWithBuffer);
+        std::memset(mInfo, 0, numBytesTotal - numElementsWithBuffer * sizeof(Node));
 
         // set sentinel
         mInfo[numElementsWithBuffer] = 1;
